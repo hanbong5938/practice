b52d14819f320e76c6bfb12c0880714c034a8a10
han <hanbong5938@gmail.com>
2023-09-06 13:19:24 +0900

    init

--- src/main/kotlin/com/example/aop/domain/user/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aop.domain.user
-
-import com.example.aop.domain.common.BaseEntity
-import jakarta.persistence.Entity
-
-@Entity(name = "user_table")
-data class User(var name: String) : BaseEntity() {
-    constructor() : this("")
-}
--- src/main/kotlin/com/example/aop/domain/user/UserService.kt
+++
@@ -1,6 +0,0 @@
-package com.example.aop.domain.user
-
-interface UserService {
-    fun signUp(name: String): User?
-    fun findAll(): MutableList<User>?
-}
